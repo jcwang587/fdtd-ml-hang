4341b9fd150b92549a8452b2be9f4e84af3003cc
Jiacheng Wang <63186896+jcwang587@users.noreply.github.com>
2025-01-19 23:05:00 -0500

    load data

--- gen2/data_pre.py
+++ gen2/data_pre.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 
-em_data = pd.read_csv('data/em_data.csv')
-fe_data = pd.read_csv('data/fe_data.csv')
-qe_data = pd.read_csv('data/qe_data.csv')
+em_data = pd.read_csv('data/em.csv')
+fe_data = pd.read_csv('data/fe.csv')
+qe_data = pd.read_csv('data/qe.csv')
+factor_data = pd.read_csv('data/factor.csv')
